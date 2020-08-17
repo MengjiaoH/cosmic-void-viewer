013e233764686328a5505bf150039f2e6ce58b8c
MengjiaoH <u1078078@utah.edu>
2020-08-16 22:35:16 -0600

    update to ospray 2.0

--- testMain.cpp
+++ testMain.cpp
@@ -1,4 +1,16 @@
-#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+// Copyright 2009-2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+/* This is a small example tutorial how to use OSPRay in an application.
+ *
+ * On Linux build it in the build_directory with
+ *   g++ ../apps/ospTutorial/ospTutorial.cpp -I ../ospray/include \
+ *       -I ../../rkcommon -L . -lospray -Wl,-rpath,. -o ospTutorial
+ * On Windows build it in the build_directory\$Configuration with
+ *   cl ..\..\apps\ospTutorial\ospTutorial.cpp /EHsc -I ..\..\ospray\include ^
+ *      -I ..\.. -I ..\..\..\rkcommon ospray.lib
+ */
+
 #include <errno.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -11,21 +23,19 @@
 
 #include <vector>
 
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-
-#include "ospray/ospray_cpp.h"
-
+// OpenGL
 #include <GL/gl3w.h>
 #include <GLFW/glfw3.h>
+
+#include "ospray/ospray_cpp.h"
 
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
 #include "callbacks.h"
-#include "ArcballCamera.h"
 #include "shader.h"
+#include "ArcballCamera.h"
 
 using namespace rkcommon::math;
 
@@ -44,7 +54,7 @@ void main(void){
 
 const std::string display_texture_fs = R"(
 #version 420 core
-uniform sampler2D img;
+layout(binding=0) uniform sampler2D img;
 out vec4 color;
 void main(void){ 
 	ivec2 uv = ivec2(gl_FragCoord.xy);
@@ -56,41 +66,71 @@ static void glfw_error_callback(int error, const char* description)
     fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
-int main(int argc, const char** argv)
+// helper function to write the rendered image as PPM file
+void writePPM(const char *fileName, const vec2i &size, const uint32_t *pixel)
 {
-	vec2i imgSize;
-    imgSize.x = 512; // width
-    imgSize.y = 512; // height
+  FILE *file = fopen(fileName, "wb");
+  if (file == nullptr) {
+    fprintf(stderr, "fopen('%s', 'wb') failed: %d", fileName, errno);
+    return;
+  }
+  fprintf(file, "P6\n%i %i\n255\n", size.x, size.y);
+  unsigned char *out = (unsigned char *)alloca(3 * size.x);
+  for (int y = 0; y < size.y; y++) {
+    const unsigned char *in =
+        (const unsigned char *)&pixel[(size.y - 1 - y) * size.x];
+    for (int x = 0; x < size.x; x++) {
+      out[3 * x + 0] = in[4 * x + 0];
+      out[3 * x + 1] = in[4 * x + 1];
+      out[3 * x + 2] = in[4 * x + 2];
+    }
+    fwrite(out, 3 * size.x, sizeof(char), file);
+  }
+  fprintf(file, "\n");
+  fclose(file);
+}
 
-	vec2i windowSize;
-	windowSize.x = imgSize.x * 2;
-	windowSize.y = imgSize.y * 2;
+int main(int argc, const char **argv)
+{
+    // image size
+    vec2i imgSize;
+    imgSize.x = 1024; // width
+    imgSize.y = 768; // height
 
-	// camera
-	vec3f cam_pos{0.f, 0.f, 0.f};
-	vec3f cam_up{0.f, 1.f, 0.f};
-	vec3f cam_view{0.1f, 0.f, 1.f};
+    // camera
+    vec3f cam_pos{0.f, 0.f, 0.f};
+    vec3f cam_up{0.f, 1.f, 0.f};
+    vec3f cam_view{0.1f, 0.f, 1.f};
 
-	// triangle mesh data
-	std::vector<vec3f> vertex = {vec3f(-1.0f, -1.0f, 3.0f),
-		vec3f(-1.0f, 1.0f, 3.0f),
-		vec3f(1.0f, -1.0f, 3.0f),
-		vec3f(0.1f, 0.1f, 0.3f)};
+    // triangle mesh data
+    std::vector<vec3f> vertex = {vec3f(-1.0f, -1.0f, 3.0f),
+        vec3f(-1.0f, 1.0f, 3.0f),
+        vec3f(1.0f, -1.0f, 3.0f),
+        vec3f(0.1f, 0.1f, 0.3f)};
 
-	std::vector<vec4f> color = {vec4f(0.9f, 0.5f, 0.5f, 1.0f),
-		vec4f(0.8f, 0.8f, 0.8f, 1.0f),
-		vec4f(0.8f, 0.8f, 0.8f, 1.0f),
-		vec4f(0.5f, 0.9f, 0.5f, 1.0f)};
+    std::vector<vec4f> color = {vec4f(0.9f, 0.5f, 0.5f, 1.0f),
+        vec4f(0.8f, 0.8f, 0.8f, 1.0f),
+        vec4f(0.8f, 0.8f, 0.8f, 1.0f),
+        vec4f(0.5f, 0.9f, 0.5f, 1.0f)};
 
-	std::vector<vec3ui> index = {vec3ui(0, 1, 2), vec3ui(1, 2, 3)};
+    std::vector<vec3ui> index = {vec3ui(0, 1, 2), vec3ui(1, 2, 3)};
 
-	printf("initialize OSPRay...");
+    box3f worldBound;
+    worldBound.lower = vec3f{-1.f, -1.f, 0.f};
+    worldBound.upper = vec3f{1.f, 1.f, 3.0f};
 
-	OSPError init_error = ospInit(&argc, argv);
-	if (init_error != OSP_NO_ERROR)
-		return init_error;
+    ArcballCamera arcballCamera(worldBound, imgSize);
+    
+    std::shared_ptr<App> app;
+    app = std::make_shared<App>(imgSize, arcballCamera);
 
-	//!! Create GLFW Window
+    // initialize OSPRay; OSPRay parses (and removes) its commandline parameters,
+    // e.g. "--osp:debug"
+    OSPError init_error = ospInit(&argc, argv);
+    if (init_error != OSP_NO_ERROR)
+        return init_error;
+    
+    //!! Create GLFW Window
     GLFWwindow* window;
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit()){
@@ -99,14 +139,13 @@ int main(int argc, const char** argv)
     }else{
         std::cout << "Initialize GLFW!! " << std::endl;
     }
-    // const char* glsl_version = "#version 130";
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 
-    window = glfwCreateWindow(windowSize.x, windowSize.y, "Viewer", NULL, NULL);
-    
-	if (!window)
+	window = glfwCreateWindow(imgSize.x, imgSize.y, "OSPRay Viewer", NULL, NULL);
+
+    if (!window)
     {
         std::cout << "Why not opening a window" << std::endl;
         glfwTerminate();
@@ -121,13 +160,11 @@ int main(int argc, const char** argv)
             fprintf(stderr, "failed to initialize OpenGL\n");
             return -1;
     }
-	
-	// Setup Dear ImGui context
+
+    // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
@@ -136,23 +173,12 @@ int main(int argc, const char** argv)
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init();
 
-	// calculate world bound to initialize camera
-    box3f worldBound;
-	worldBound.lower = vec3f(-1.0f, -1.0f, 0.0f);
-	worldBound.upper = vec3f(1.0f, 1.0f, 3.0f);
-
-	//!! Camera
-    ArcballCamera arcballCamera(worldBound, imgSize);
-    //! App used for control user interface  
-	std::shared_ptr<App> app;
-    app = std::make_shared<App>(imgSize, arcballCamera);
-	
-	Shader display_render(fullscreen_quad_vs, display_texture_fs);
+    Shader display_render(fullscreen_quad_vs, display_texture_fs);
 
 	GLuint render_texture;
 	glGenTextures(1, &render_texture);
 	glBindTexture(GL_TEXTURE_2D, render_texture);
-	glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA8, windowSize.x, windowSize.y);
+	glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA8, imgSize.x, imgSize.y);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
@@ -166,100 +192,102 @@ int main(int argc, const char** argv)
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glDisable(GL_DEPTH_TEST);
 
-	{
-        std::cout << "debug0" << std::endl;
-		// create and setup camera
-		ospray::cpp::Camera camera("perspective");
-		camera.setParam("aspect", imgSize.x / (float)imgSize.y);
-		camera.setParam("position", cam_pos);
-		camera.setParam("direction", cam_view);
-		camera.setParam("up", cam_up);
-		camera.commit(); // commit each object to indicate modifications are done
-    
-		// create and setup model and mesh
-		ospray::cpp::Geometry mesh("mesh");
-		mesh.setParam("vertex.position", ospray::cpp::CopiedData(vertex));
-		mesh.setParam("vertex.color", ospray::cpp::CopiedData(color));
-		mesh.setParam("index", ospray::cpp::CopiedData(index));
-		mesh.commit();
+    // use scoped lifetimes of wrappers to release everything before ospShutdown()
+    {
+        // create and setup camera
+        ospray::cpp::Camera camera("perspective");
+        camera.setParam("aspect", imgSize.x / (float)imgSize.y);
+        camera.setParam("position", arcballCamera.eyePos());
+        camera.setParam("direction", arcballCamera.lookDir());
+        camera.setParam("up", arcballCamera.upDir());
+        camera.commit(); // commit each object to indicate modifications are done
 
-		// put the mesh into a model
-		ospray::cpp::GeometricModel model(mesh);
-		model.commit();
+        // create and setup model and mesh
+        ospray::cpp::Geometry mesh("mesh");
+        mesh.setParam("vertex.position", ospray::cpp::CopiedData(vertex));
+        mesh.setParam("vertex.color", ospray::cpp::CopiedData(color));
+        mesh.setParam("index", ospray::cpp::CopiedData(index));
+        mesh.commit();
 
-		// put the model into a group (collection of models)
-		ospray::cpp::Group group;
-		group.setParam("geometry", ospray::cpp::CopiedData(model));
-		group.commit();
+        // put the mesh into a model
+        ospray::cpp::GeometricModel model(mesh);
+        model.commit();
 
-		// put the group into an instance (give the group a world transform)
-		ospray::cpp::Instance instance(group);
-		instance.commit();
+        // put the model into a group (collection of models)
+        ospray::cpp::Group group;
+        group.setParam("geometry", ospray::cpp::CopiedData(model));
+        group.commit();
 
-		// put the instance in the world
-		ospray::cpp::World world;
-		world.setParam("instance", ospray::cpp::CopiedData(instance));
+        // put the group into an instance (give the group a world transform)
+        ospray::cpp::Instance instance(group);
+        instance.commit();
 
-		// create and setup light for Ambient Occlusion
-		ospray::cpp::Light light("ambient");
-		light.commit();
+        // put the instance in the world
+        ospray::cpp::World world;
+        world.setParam("instance", ospray::cpp::CopiedData(instance));
 
-		world.setParam("light", ospray::cpp::CopiedData(light));
-		world.commit();
+        // create and setup light for Ambient Occlusion
+        ospray::cpp::Light light("ambient");
+        light.commit();
 
-		// create renderer, choose Scientific Visualization renderer
-		ospray::cpp::Renderer renderer("scivis");
+        world.setParam("light", ospray::cpp::CopiedData(light));
+        world.commit();
 
-		// complete setup of renderer
-		renderer.setParam("pixelSamples", 10);
-		renderer.setParam("aoSamples", 1);
-		renderer.setParam("backgroundColor", 1.0f); // white, transparent
-		renderer.commit();
+        // create renderer, choose Scientific Visualization renderer
+        ospray::cpp::Renderer renderer("scivis");
 
-		// create and setup framebuffer
-		ospray::cpp::FrameBuffer framebuffer(imgSize.x, imgSize.y, OSP_FB_SRGBA, OSP_FB_COLOR | OSP_FB_ACCUM);
-		framebuffer.clear();
-		
-		glfwSetWindowUserPointer(window, app.get());
-    	glfwSetCursorPosCallback(window, cursorPosCallback);
+        // complete setup of renderer
+        renderer.setParam("aoSamples", 1);
+        renderer.setParam("backgroundColor", 1.0f); // white, transparent
+        renderer.commit();
 
-		framebuffer.renderFrame(renderer, camera, world);
-		uint32_t *fb = (uint32_t *)framebuffer.map(OSP_FB_COLOR);
+        // create and setup framebuffer
+        ospray::cpp::FrameBuffer framebuffer(imgSize.x, imgSize.y, OSP_FB_SRGBA, OSP_FB_COLOR | OSP_FB_ACCUM);
+        framebuffer.clear();
 
-		std::string filename = "/home/mengjiao/Desktop/projects/time-varying-data-viewer/image.jpg";
-		
-		while (!glfwWindowShouldClose(window)){
-			if (app ->isCameraChanged) {
-				camera.setParam("position", cam_pos);
-				camera.setParam("direction", cam_view);
-				camera.setParam("up", cam_up);
-				camera.commit();
-				framebuffer.clear();
-				app ->isCameraChanged = false;
-        	}
-            // 
-            // app->camera.lookDir()
-            // app->camera.upDir()
-			
-			// render one frame
-    		framebuffer.renderFrame(renderer, camera, world);
-			uint32_t *fb = (uint32_t *)framebuffer.map(OSP_FB_COLOR);
-        
-			glViewport(0, 0, windowSize.x, windowSize.y);
-			glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, windowSize.x, windowSize.y, GL_RGB, GL_UNSIGNED_BYTE, sr_fb);
+        glfwSetWindowUserPointer(window, app.get());
+        glfwSetCursorPosCallback(window, cursorPosCallback);
 
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-			glUseProgram(display_render.program);
-			glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        while (!glfwWindowShouldClose(window))
+        {
 
-        	framebuffer.unmap(fb);
+            if (app ->isCameraChanged) {
+                camera.setParam("position", app->camera.eyePos());
+                camera.setParam("direction", app->camera.lookDir());
+                camera.setParam("up", app->camera.upDir());
+                camera.commit();
+                framebuffer.clear();
+                app ->isCameraChanged = false;
+            }
+            // render one frame
+            framebuffer.renderFrame(renderer, camera, world);
+            framebuffer.clear();
 
-	        glfwSwapBuffers(window);
-    	    glfwPollEvents();
-		}
-	}
-	glfwDestroyWindow(window);
+            // access framebuffer and write its content as PPM file
+            uint32_t *fb = (uint32_t *)framebuffer.map(OSP_FB_COLOR);
+            glViewport(0, 0, imgSize.x, imgSize.y);
+		    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, imgSize.x, imgSize.y, GL_RGBA, GL_UNSIGNED_BYTE, fb);
+
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glUseProgram(display_render.program);
+            glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+            framebuffer.unmap(fb);
+
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
     glfwTerminate();
+
+    ospShutdown();
 
     return 0;
 }
--- utils/ArcballCamera.h
+++ utils/ArcballCamera.h
@@ -1,34 +1,19 @@
-// ======================================================================== //
-// Copyright 2017-2019 Intel Corporation                                    //
-//                                                                          //
-// Licensed under the Apache License, Version 2.0 (the "License");          //
-// you may not use this file except in compliance with the License.         //
-// You may obtain a copy of the License at                                  //
-//                                                                          //
-//     http://www.apache.org/licenses/LICENSE-2.0                           //
-//                                                                          //
-// Unless required by applicable law or agreed to in writing, software      //
-// distributed under the License is distributed on an "AS IS" BASIS,        //
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
-// See the License for the specific language governing permissions and      //
-// limitations under the License.                                           //
-// ======================================================================== //
+// Copyright 2017-2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
 
 #pragma once
 
-
-#include "rkcommon/math/rkmath.h"
 #include "rkcommon/math/AffineSpace.h"
-#include "rkcommon/math/vec.h"
 
 class ArcballCamera
 {
  public:
   ArcballCamera(const rkcommon::math::box3f &worldBounds,
-                const rkcommon::math::vec2i &windowSize);
+      const rkcommon::math::vec2i &windowSize);
 
   // All mouse positions passed should be in [-1, 1] normalized screen coords
-  void rotate(const rkcommon::math::vec2f &from, const rkcommon::math::vec2f &to);
+  void rotate(
+      const rkcommon::math::vec2f &from, const rkcommon::math::vec2f &to);
   void zoom(float amount);
   void pan(const rkcommon::math::vec2f &delta);
 
@@ -37,8 +22,9 @@ class ArcballCamera
   rkcommon::math::vec3f lookDir() const;
   rkcommon::math::vec3f upDir() const;
 
+  void setRotation(rkcommon::math::quaternionf);
+
   void updateWindowSize(const rkcommon::math::vec2i &windowSize);
-  rkcommon::math::vec2f worldToPixel(rkcommon::math::vec3f worldPos, rkcommon::math::vec2i imgSize);
 
  protected:
   void updateCamera();
@@ -50,5 +36,4 @@ class ArcballCamera
   rkcommon::math::vec2f invWindowSize;
   rkcommon::math::AffineSpace3f centerTranslation, translation, invCamera;
   rkcommon::math::quaternionf rotation;
-
 };
